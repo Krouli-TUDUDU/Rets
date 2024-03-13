826d28d9f6108ccc12160b837ea34a8f5570f399
Krouli-TUDUDU <132000628+Krouli-TUDUDU@users.noreply.github.com>
2024-03-13 19:38:38 +0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void voo()
+        {
+
+        }
+        
+
+        private void comand1(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void comand2(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void comand(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void comand5(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+
+}
---
+++ NewPage1.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace MauiApp1
+{
+    public partial class NewPage1 : ContentPage
+    {
+        int count = 0;
+
+        public NewPage1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Vpered()
+        {
+            Dog.Source = "dog1.jpg";
+        }
+        private void Nazad()
+        { 
+            Dog.Source = "dog.jpg";         
+        }
+    }
+}
